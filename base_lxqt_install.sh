9817659797994482b68d64e9ae84c8ad0f7b12d3
Md Shahi Amran Hossain <mhossain@inky.per>
2023-05-13 19:41:54 +0200

    config updated

---
+++ base_lxqt_install.sh
@@ -0,0 +1,21 @@
+# !/bin/bash
+
+# install base pakages
+sudo apt install build-essential git -y
+
+# install devel pakages
+sudo apt install net-tools libncurses5-dev libssl-dev flex bison autoconf libtool libtinfo5 libncurses5-dev libncursesw5-dev libncurses5 libtinfo-dev  -y
+
+# python pakages
+sudo apt install  python3 python3-pexpect python3-pip python3-git python3-jinja2 -y
+
+# install bluetooth required pakagesq
+sudo apt install bluetooth bluedevil bluez bluez-tools pulseaudio-module-bluetooth blueman -y
+
+
+# install other pakages
+sudo apt install redshift redshift-gtk neofetch cmatrix feh lxappearance kitty htop exa rofi lxappearance flatpak -y
+
+# Install fonts
+sudo apt install fonts-font-awesome fonts-powerline fonts-ubuntu fonts-liberation2 fonts-liberation fonts-terminus fonts-cascadia-code
+
