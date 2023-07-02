d726dfc2ea92bd6be2f3631dec588549eee78cbc
Md Shahi Amran Hossain <mhossain@localhost.localdomain>
2023-07-02 14:49:22 +0200

    opensuse post install

---
+++ installer_suse.sh
@@ -0,0 +1,8 @@
+# this will work only after install with a desktop
+
+# system pakages
+sudo zypper install -t pattern base devel_basis devel_C_C++ devel_java devel_kernel devel_python3 devel_rpm_build  multimedia 
+
+
+# fav pakages
+sudo zypper install i3 picom redshift-gtk lxappearance polybar kitty password-store QtPass gparted rofi exa feh geany htop neofetch flameshot flatpak 
